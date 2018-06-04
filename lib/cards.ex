aa651ff87e1e11d078b1321451f3c0f02650eb6f
Pyae Phyo Hlaing <pyaephyohlaing.5@gmail.com>
2018-06-04 18:27:11 +0630

    FirstCommit

---
+++ lib/cards.ex
@@ -0,0 +1,63 @@
+defmodule Cards do
+
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards.
+  """
+ 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
+
+  def create_deck do
+      values = ["Ace" , "Two" , "Three"]
+      suits = ["Spades" , "Clubs" , "Hearts" , "Diamonds"]
+
+      for value <- values , suit <- suits do
+          "#{value} of #{suit}"
+      end  
+  end
+
+  def shuffle(deck) do
+      Enum.shuffle(deck)
+  end
+
+  def contains?(deck,card) do
+      Enum.member?(deck,card) 
+  end
+
+  @doc """ 
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be
+    in the hand.
+
+    ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace Of Spades"]
+     
+    """
+
+  def deal(deck, hand_size) do
+      Enum.split(deck,hand_size)   #{ *hand* , *deck*}
+  end
+
+  def save(deck, filename) do
+      binary = :erlang.term_to_binary(deck)
+        File.write(filename, binary)
+  end
+
+  def load(filename) do
+        case File.read(filename) do
+            {:ok, binary} -> :erlang.binary_to_term(binary)
+            {:error, _reason} -> "That file does not exit"
+        end
+  end
+
+  def create_hand(hand_size) do
+        Cards.create_deck
+        |> Cards.shuffle
+        |> Cards.deal(hand_size)
+  end
+end
