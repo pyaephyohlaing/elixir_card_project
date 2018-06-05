661a635d66d567569f9c1a460c560406224ef56d
Pyae Phyo Hlaing <pyaephyohlaing.5@gmail.com>
2018-06-05 15:34:07 +0630

    Last Unit

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
-
-  test "greets the world" do
-    assert Cards.hello() == :world
+  
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 12
+  end
+    
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
   end
 end
